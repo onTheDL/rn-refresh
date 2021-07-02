fc8fe61efd5d3c7785e5c6d70fe1506dcec7b686
Daniel Laserna <daniel.laserna@protonmail.com>
2021-07-01 22:33:55 -0400

    Add text input and button

--- App.js
+++ App.js
@@ -1,19 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 
 export default function App() {
-
-  const [text, setText] = useState('Open up App.js to start working on your app!')
-
-  const handleChange = () => {
-    setText('Changed')
-  }
   return (
-    <View style={styles.container}>
-      <Text>{text}</Text>
-      <StatusBar style="auto" />
-      <Button title="Change Text" onPress={handleChange} />
+    <View style={{ padding: 60 }}>
+      <View style={styles.flexRow}>
+        <TextInput
+          placeholder="Course Goal"
+          style={{
+            borderColor: "black",
+            borderWidth: 1,
+            width: '80%',
+      
+          }}
+        />
+
+        <Button title="ADD" />
+      </View>
+      <View />
     </View>
   );
 }
@@ -21,8 +26,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  flexRow: {
+    flexDirection: 'row',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-between'
   },
 });

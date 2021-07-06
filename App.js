ad1016db749f2138d8b13d9266b087623696bf24
Daniel Laserna <daniel.laserna@protonmail.com>
2021-07-06 16:02:07 -0400

    Add basic goals list

--- App.js
+++ App.js
@@ -3,36 +3,51 @@ import React, { useState } from "react";
 import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 
 export default function App() {
+  const [goal, setGoal] = useState("");
+  const [courseGoals, setCourseGoals] = useState([])
+
+  const addGoalHandler = () => {
+    setCourseGoals(prevGoals => [...prevGoals, goal], console.log(courseGoals))
+    
+  }
+
   return (
-    <View style={{ padding: 60 }}>
-      <View style={styles.flexRow}>
+    <View style={styles.container}>
+      <View style={styles.inputContainer}>
         <TextInput
           placeholder="Course Goal"
-          style={{
-            borderColor: "black",
-            borderWidth: 1,
-            width: '80%',
-      
-          }}
+          style={styles.input}
+          onChangeText={(input) => setGoal(input)}
+          value={goal}
         />
 
-        <Button title="ADD" />
+        <Button title="ADD" onPress={addGoalHandler} />
       </View>
-      <View />
+      <View>
+        
+          {courseGoals.map(courseGoal => (
+            <Text key={courseGoal}>{courseGoal}</Text>
+          ))}
+        
+        
+      </View>
+      
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    padding: 50,
   },
-  flexRow: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between'
+  inputContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  input: {
+    borderColor: "black",
+    borderWidth: 1,
+    width: "80%",
   },
 });
